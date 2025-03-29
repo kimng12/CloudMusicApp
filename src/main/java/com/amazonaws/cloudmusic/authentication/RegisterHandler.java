50fcb0c7eeb95d511cab007f14b9ba17139ac23c
= <=>
2025-03-29 07:20:32 +0000

    Added the use of enviornment file for access key and secret access key Also package clean fiailing so added new version of web archive plugin Additionally the webservlet should work now

--- src/main/java/com/amazonaws/cloudmusic/authentication/RegisterHandler.java
+++ src/main/java/com/amazonaws/cloudmusic/authentication/RegisterHandler.java
@@ -1,9 +1,16 @@
 package com.amazonaws.cloudmusic.authentication;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.List;
 import java.util.Map;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -11,20 +18,41 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+
 public class RegisterHandler {
     private final DynamoDB dynamoDB;
     private final Table loginTable;
 
     public RegisterHandler() {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        AmazonDynamoDB client = null;
+        try (InputStream input =  getClass().getClassLoader().getResourceAsStream("aws-credentials.properties")) {
+            if (input != null) {
+                Properties properties = new Properties();
+                properties.load(input);
+                String accessKey = properties.getProperty("aws_access_key_id");
+                String secretKey = properties.getProperty("aws_secret_access_key");
+                BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+                client = AmazonDynamoDBClientBuilder.standard()
+                        .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                        .withRegion(Regions.US_EAST_1)
+                        .build();
+            }else{
+                client = AmazonDynamoDBClientBuilder.standard()
                 .withRegion(Regions.US_EAST_1)
                 .build();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (client == null) {
+            throw new IllegalStateException("DynamoDB client could not be initialized");
+        }
         this.dynamoDB = new DynamoDB(client);
         this.loginTable = dynamoDB.getTable("login");
     }
 
     // Creates user with given email, password, username and subscription status
-    public String putItemInTable(String email, String password,String user_name) {
+    public String registerUser(String email, String password,String user_name) {
         String message = "";
         try {
             
@@ -43,9 +71,8 @@ public class RegisterHandler {
                 message = "The email already exists";
             }
         } catch (Exception e) {
-            message = "Register error: \" + e.getMessage()";
+            message = "Register error: " + e.getMessage();
         }
         return message;
     }
-
 }
---
+++ src/main/java/com/amazonaws/cloudmusic/webapp/RegisterServlet.java
@@ -0,0 +1,41 @@
+package com.amazonaws.cloudmusic.webapp;
+import com.amazonaws.cloudmusic.authentication.RegisterHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet{
+    private RegisterHandler registerHandler;
+    @Override
+    public void init() {
+        registerHandler = new RegisterHandler();
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            String user_name = request.getParameter("user_name");
+            String message = registerHandler.registerUser(email, password,user_name);
+            if (message.contains("Succesfully added user")){
+                response.sendRedirect("login.html"); 
+            }else{
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);  // 400 Bad Request
+                response.setContentType("text/html");
+                PrintWriter writer = response.getWriter();
+                writer.append("Registration failed: " + message);
+            }
+        }catch (IOException e){
+            response.setStatus(500);
+            response.setContentType("text/xml");
+            PrintWriter writer=response.getWriter();
+            writer.append("Registration failed: "+e.getMessage() );
+        }
+
+    }
+}
