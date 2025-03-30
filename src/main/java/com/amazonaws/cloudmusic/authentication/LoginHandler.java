662e107678b6c5a9d582ece2ff509c42b62678de
= <=>
2025-03-30 08:40:14 +0000

    Login also made to use keys from aws-credentials.properties file

--- src/main/java/com/amazonaws/cloudmusic/authentication/LoginHandler.java
+++ src/main/java/com/amazonaws/cloudmusic/authentication/LoginHandler.java
@@ -1,11 +1,17 @@
 package com.amazonaws.cloudmusic.authentication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 
 public class LoginHandler {
@@ -13,9 +19,30 @@ public class LoginHandler {
     private final Table loginTable;
 
     public LoginHandler() {
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withRegion(Regions.US_EAST_1) // adjust if needed
+        AmazonDynamoDB client = null;
+        try (InputStream input =  getClass().getClassLoader().getResourceAsStream("aws-credentials.properties")) {
+            if (input != null) {
+                Properties properties = new Properties();
+                properties.load(input);
+                String accessKey = properties.getProperty("aws_access_key_id");
+                String secretKey = properties.getProperty("aws_secret_access_key");
+                String sessionToken = properties.getProperty("aws_session_token");
+                BasicSessionCredentials awsCreds = new BasicSessionCredentials(accessKey, secretKey,sessionToken);
+                client = AmazonDynamoDBClientBuilder.standard()
+                        .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                        .withRegion(Regions.US_EAST_1)
+                        .build();
+            }else{
+                client = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(Regions.US_EAST_1)
                 .build();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (client == null) {
+            throw new IllegalStateException("DynamoDB client could not be initialized");
+        }
         this.dynamoDB = new DynamoDB(client);
         this.loginTable = dynamoDB.getTable("login");
     }
